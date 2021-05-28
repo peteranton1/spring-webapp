c86686fdc5e5839890b029b7eee5364a359ba650
peteranton1 <peteranton1@yahoo.com>
2021-05-28 21:10:43 +0100

    Initial2

---
+++ src/main/java/com/example/webapp/SpringWebappApplication.java
@@ -0,0 +1,13 @@
+package com.example.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebappApplication.class, args);
+	}
+
+}
